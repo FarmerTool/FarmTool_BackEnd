87053e0479f223b17c3062940ecb80b94ae330a8
piper006 <arxontispipertzisoff@gmail.com>
2021-03-10 18:05:01 +0200

    init

---
+++ src/main/java/com/pipertzis/FarmHelper_BackEnd/FarmHelperBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.pipertzis.FarmHelper_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmHelperBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmHelperBackEndApplication.class, args);
+	}
+
+}
