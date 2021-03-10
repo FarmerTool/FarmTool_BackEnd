2231477cad9a8cc70ce1e80debb7bcc35f7e21f7
piper006 <arxontispipertzisoff@gmail.com>
2021-03-10 18:23:29 +0200

    Java version change

--- src/main/java/com/pipertzis/FarmHelper_BackEnd/FarmHelperBackEndApplication.java
+++ src/main/java/com/pipertzis/FarmHelper_BackEnd/FarmHelperBackEndApplication.java
@@ -2,6 +2,8 @@ package com.pipertzis.FarmHelper_BackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class FarmHelperBackEndApplication {
@@ -9,5 +11,5 @@ public class FarmHelperBackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FarmHelperBackEndApplication.class, args);
 	}
-
+	
 }
