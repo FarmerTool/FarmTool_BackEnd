a6da778ced79d612cd1a8c585ee747a27e4d30fb
piper006 <arxontispipertzisoff@gmail.com>
2021-10-17 18:43:00 +0300

    Implemented Spring Swagger and Swagger-UI

--- src/main/java/com/pipertzis/FarmHelper_BackEnd/FarmHelperBackEndApplication.java
+++ src/main/java/com/pipertzis/FarmHelper_BackEnd/FarmHelperBackEndApplication.java
@@ -3,10 +3,11 @@ package com.pipertzis.FarmHelper_BackEnd;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
+@EnableSwagger2
 public class FarmHelperBackEndApplication {
 
 	public static void main(String[] args) {
